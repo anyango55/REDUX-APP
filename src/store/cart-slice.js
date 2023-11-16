d361d836e462a9f5e78203b9e92485bdb933df40
anyango55 <anyangoc55@gmail.com>
2023-11-16 11:20:16 +0300

    Add to cart functinality

---
+++ src/store/cart-slice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        itemsList: [],
+        totalquantity: 0,
+        showCart: false
+    },
+    reducers: {
+        addToCart(state, action) {
+            const newItem = action.payload;
+            // to check if item is already avilable
+            const existingItem = state.itemsList.find((item)=> item.id === newItem.id);
+
+            if(existingItem){
+                existingItem.quantity++;
+                existingItem.price+= newItem.price;
+            } else{
+                state.itemsList.push({
+                    id: newItem.id,
+                    price:newItem.price,
+                    quantity:1,
+                    totalPrice:newItem.price,
+                    name: newItem.name
+                })
+            }
+        },
+
+        removeFromCart() {
+
+        },
+
+        setShowCart(state) {
+            state.showCart = true;
+        },
+
+    }
+})
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
