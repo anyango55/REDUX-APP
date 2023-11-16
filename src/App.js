7d81f90dfec5dc408d622086dff98f2912d252fa
anyango55 <anyangoc55@gmail.com>
2023-11-16 10:49:16 +0300

    frontend

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
+
+function App() {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  
+  return (
+    <div className="App">
+      {!isLoggedIn && <Auth />}
+
+      {isLoggedIn &&  <Layout />}
+
+      
+      {/* <Layout /> */}
+    </div>
+  );
+}
+
+export default App;
